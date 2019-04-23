eb8a8577fb96dafe195501ab5469fcbe8b11cc0e
Chase Ward <chward@rs-f251-m09.local>
2019-04-23 08:07:55 -0500

    Initial Commit

---
+++ capstone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  capstone
+//
+//  Created by Chase Ward on 4/23/19.
+//  Copyright © 2019 Chase Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
