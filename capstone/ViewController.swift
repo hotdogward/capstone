b72173902c6634ec173e172baa1b0cea18634bf1
Chase Ward <chward@rs-f251-m09.local>
2019-04-25 08:14:29 -0500

    Push to remote

--- capstone/ViewController.swift
+++ capstone/ViewController.swift
@@ -12,9 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+ 
+        // Create a DatePicker
+        let datePicker: UIDatePicker = UIDatePicker()
+        
+        // Position date picket within a view
+        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200)
+        
+        // Set some of UIDatePicker properties
+        datePicker.backgroundColor = UIColor.white
+        
+        // Add an event to call onDidChangeDate function when value is changed
+        datePicker.addTarget(self, action: #selector(ViewController.datePickerValueChanged(_:)), for: .valueChanged)
+        
+        // Add datePicker to the view
+        self.view.addSubview(datePicker)
+    
+   
+    func didPickerValueChanged(_ sender: UIDatePicker){
+        
+        // Create date formatter
+        let dateFormatter: DateFormatter = DateFormatter()
+        
+        // Set date format
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        
+        // Apply date format
+        let selectedDate: String = dateFormatter.string(from: sender.date)
+        
+        print("Selected value \(selectedDate)")
+        }
+        
+        func didRecieveMemoryWarning(){
+            super.didReceiveMemoryWarning()
+            
+           }
     }
-
-
 }
-
